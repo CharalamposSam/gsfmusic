5db567845f1811d72bbb0f27a33f8b1641cce1f2
charalampos <chara.lampos.samou.ridis@gmail.com>
2023-01-16 21:55:25 +0200

    restore test

--- conn.php
+++ conn.php
@@ -1,3 +1,3 @@
 <?php
 $conn = mysqli_connect( 'localhost', 'u174548577_charalampos', '386900072422211Le', 'u174548577_url_shortener' );
-// $conn = mysqli_connect( 'localhost', 'root', '', 'url_shortener' ); aaaaaaaaaaaaaaaaaaaaa
+// $conn = mysqli_connect( 'localhost', 'root', '', 'url_shortener' );
