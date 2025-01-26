d3543016901ec271f90992dd16d4dcda065a88c4
charalampos <chara.lampos.samou.ridis@gmail.com>
2025-01-26 03:43:31 +0200

    fix header mobile version

--- header.php
+++ header.php
@@ -10,28 +10,33 @@ if (strpos($_SERVER['REQUEST_URI'], '/album/') !== false || strpos($_SERVER['REQ
 <link rel="stylesheet" href="<?php echo $basePath; ?>css/header.css" />
 
 <header>
-    <a href="https://gsfmusic.gr" title="GSF Music" class="logo" class="iconHover">
-        <img src="<?php echo $basePath; ?>icons/GSF_logo_white.png" alt="Αρχική">
-    </a>
-    <a target="_blank" href="https://www.gsfmusic.gr/eshop" title="E-shop" class="iconHover">
-        <img src="<?php echo $basePath; ?>icons/nav-eshop.png" alt="eshop">
-    </a>
-    <a target="_blank" href="https://www.facebook.com/gsfmusicofficial" title="Facebook" class="iconHover">
-        <img src="<?php echo $basePath; ?>icons/facebook.png" alt="facebook">
-    </a>
-    <a target="_blank" href="https://www.instagram.com/gsfmusicofficial/" title="Instagram" class="iconHover">
-        <img src="<?php echo $basePath; ?>icons/instagram.png" alt="youtube">
-    </a>
-    <a target="_blank" href="https://www.youtube.com/channel/UCUTJlf84CFiq-eUU0M6Vf3A?sub_confirmation=1%C2%A8" title="YouTube" class="iconHover">
-        <img src="<?php echo $basePath; ?>icons/youtube-icon.png" alt="youtube">
-    </a>
-    <a target="_blank" href="https://www.tiktok.com/@gsfmusic" title="TikTOk" class="iconHover">
-        <img src="<?php echo $basePath; ?>icons/tiktok-icon.png" alt="tiktok">
-    </a>
-    <a href="" title="Email" class="emailMenuIcon">
-        <img src="<?php echo $basePath; ?>icons/email.png" alt="email">
-        <span class="emailTag">csamouridis@gsfmusic.gr</span>
-    </a>
+    <div class="logo">
+        <a href="https://gsfmusic.gr" title="GSF Music" class="logo" class="iconHover">
+            <img src="<?php echo $basePath; ?>icons/GSF_logo_white.png" alt="Αρχική">
+        </a>
+    </div>
+    <div class="links">
+        <a target="_blank" href="https://www.gsfmusic.gr/eshop" title="E-shop" class="iconHover">
+            <img src="<?php echo $basePath; ?>icons/nav-eshop.png" alt="eshop">
+        </a>
+        <a target="_blank" href="https://www.facebook.com/gsfmusicofficial" title="Facebook" class="iconHover">
+            <img src="<?php echo $basePath; ?>icons/facebook.png" alt="facebook">
+        </a>
+        <a target="_blank" href="https://www.instagram.com/gsfmusicofficial/" title="Instagram" class="iconHover">
+            <img src="<?php echo $basePath; ?>icons/instagram.png" alt="youtube">
+        </a>
+        <a target="_blank" href="https://www.youtube.com/channel/UCUTJlf84CFiq-eUU0M6Vf3A?sub_confirmation=1%C2%A8" title="YouTube" class="iconHover">
+            <img src="<?php echo $basePath; ?>icons/youtube-icon.png" alt="youtube">
+        </a>
+        <a target="_blank" href="https://www.tiktok.com/@gsfmusic" title="TikTOk" class="iconHover">
+            <img src="<?php echo $basePath; ?>icons/tiktok-icon.png" alt="tiktok">
+        </a>
+        <a href="" title="Email" class="emailMenuIcon">
+            <img src="<?php echo $basePath; ?>icons/email.png" alt="email">
+            <span class="emailTag">csamouridis@gsfmusic.gr</span>
+        </a>
+    </div>
+    
 </header>
 
 <link rel="stylesheet" href="<?php echo $basePath; ?>css/cart.css" />
